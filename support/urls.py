02528b9ba7a926236748ad35f8275a7c69d48a29
Ksenz Vyachsleav <ksenz@MacBook-Air-Ksenz.local>
2016-06-09 22:49:03 +0300

    Исправил формирование url на правильные

--- support/urls.py
+++ support/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     url(r'^$', views.root, name='home'),
     url(r'^my_issues/?$', views.my_issues, name='my-issues'),
     url(r'^unresolved_issues/?$', views.unresolved_issues, name='unresolved-issues'),
-    url(r'^issue/([0-9]+)/?$', views.issue),
-    url(r'^create_issue/?$', views.create_issue),
+    url(r'^issue/([0-9]+)/?$', views.issue, name='issue'),
+    url(r'^create_issue/?$', views.create_issue, name='create-issue'),
 ]
