7d7879ad61f3dabb899576d858e0c0753af10447
Ksenz Vyachsleav <ksenz@MacBook-Air-Ksenz.local>
2016-06-13 13:04:42 +0300

    Добавил смену пароля, поправил маршруты

--- support/urls.py
+++ support/urls.py
@@ -6,13 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^accounts/register', views.register, name='register'),
-    #url(r'^accounts/logout', views.logoutview, name='logout'),
-    url(r'^accounts/password_reset/done', auth_views.password_reset_done, name='password-reset-done'),
-    url(r'^accounts/password_reset', auth_views.password_reset, name='password-reset'),
-    #url(r'^accounts/login', views.logout, name='logout'),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    #url(r'^', include('django.contrib.auth.urls')),
-    #url(r'^accounts/login/$', auth_views.login),
     url(r'^admin/?', admin.site.urls),
     url(r'^$', views.root, name='home'),
     url(r'^issues/?$', views.issues, name='issues'),
